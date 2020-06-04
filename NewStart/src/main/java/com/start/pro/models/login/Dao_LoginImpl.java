2e76c84d0e548c4cfcc086d145664e1c0eb898e6
ckadl0118@gmail.com <ckadl0118@gmail.com>
2020-06-04 15:18:08 +0900

    2020.06.04 사용자 : 김참이 내   용 : 로그인부분 model 완성

--- NewStart/src/main/java/com/start/pro/models/login/Dao_LoginImpl.java
+++ NewStart/src/main/java/com/start/pro/models/login/Dao_LoginImpl.java
@@ -2,90 +2,94 @@ package com.start.pro.models.login;
 
 import java.util.Map;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
 import com.start.pro.dto.DTO_User;
 
+@Repository
 public class Dao_LoginImpl implements IDao_Login {
 
+	@Autowired
+	private SqlSessionTemplate session;
 	
+	@Autowired
+	private PasswordEncoder passwordencoder;
+	
+	private final String NS = "com.start.pro.login.";
 	
 	
 	@Override
 	public DTO_User getPW(String user_email) {
-		// TODO Auto-generated method stub
-		return null;
+		return session.selectOne(NS+"getPW",user_email);
 	}
 
 	@Override
 	public DTO_User getUser(String user_seq) {
-		// TODO Auto-generated method stub
-		return null;
+		return session.selectOne(NS+"getUser", user_seq);
 	}
 
 	@Override
 	public boolean loginUpdate(String user_email) {
-		// TODO Auto-generated method stub
-		return false;
+		return (session.update(NS+"loginUpdate",user_email) > 0 ) ? true : false;
 	}
 
 	@Override
 	public boolean changeSleep(String user_seq) {
-		// TODO Auto-generated method stub
-		return false;
+		return (session.update(NS+"changeSleep",user_seq) > 0) ? true : false;
 	}
 
 	@Override
 	public boolean plusPWFail(String user_email) {
-		// TODO Auto-generated method stub
-		return false;
+		return (session.update(NS+"plusPWFail",user_email) > 0 ) ? true : false;
 	}
 
 	@Override
 	public String getPWFail(String user_email) {
-		// TODO Auto-generated method stub
-		return null;
+		return session.selectOne(NS+"getPWFail", user_email);
 	}
 
 	@Override
 	public boolean MultipleChk(Map<String, String> map) {
-		// TODO Auto-generated method stub
-		return false;
+		return ((Integer)session.selectOne(NS+"MultipleChk", map) > 0 ) ? true : false;
 	}
 
 	@Override
 	public boolean signUp(DTO_User userDto) {
-		// TODO Auto-generated method stub
-		return false;
+		String enPw = passwordencoder.encode(userDto.getUser_pw());
+		userDto.setUser_pw(enPw);
+		return (session.insert(NS+"signUp", userDto) > 0 )? true : false;
+	}
+	
+	@Override
+	public boolean signUpLog(String user_seq) {
+		return session.insert(NS+"singUpLog", user_seq) > 0 ? true : false;
 	}
 
-	@Override
-	public boolean singUpLog(String user_seq) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
 	@Override
 	public boolean changeNomal(String user_seq) {
-		// TODO Auto-generated method stub
-		return false;
+		return session.update(NS+"changeNomal",user_seq) > 0 ? true : false;
 	}
 
 	@Override
 	public String findId(String user_phone) {
-		// TODO Auto-generated method stub
-		return null;
+		return session.selectOne(NS+"findId", user_phone);
 	}
 
 	@Override
 	public boolean changePW(Map<String, String> map) {
-		// TODO Auto-generated method stub
-		return false;
+		return session.update(NS+"changePW", map) > 0 ? true : false;
 	}
 
 	@Override
 	public boolean updatePWLog(String user_email) {
-		// TODO Auto-generated method stub
-		return false;
+		return session.update(NS+"updatePWLog", user_email) > 0 ? true : false;
 	}
+
+
 
 	
 }
