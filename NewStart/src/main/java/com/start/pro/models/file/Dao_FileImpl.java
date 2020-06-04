55e8ba57dd61906c01bc7cc3e7db37a525aff1ac
leewkdrns2@naver.com <leewkdrns2@naver.com>
2020-06-04 20:42:11 +0900

    작성일 : 2020. 06. 04 작성자 : 이민규 내   용 : Configuration 수정, Model 설정

---
+++ NewStart/src/main/java/com/start/pro/models/file/Dao_FileImpl.java
@@ -0,0 +1,18 @@
+package com.start.pro.models.file;
+
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import com.start.pro.dto.DTO_User;
+import com.start.pro.security.Sc_User;
+
+@Repository
+public class Dao_FileImpl implements IDao_File {
+
+
+	
+}
---
+++ NewStart/src/main/java/com/start/pro/models/user/IDao_User.java
@@ -0,0 +1,14 @@
+package com.start.pro.models.user;
+
+import java.util.Map;
+
+import com.start.pro.dto.DTO_User;
+import com.start.pro.security.Sc_User;
+
+public interface IDao_User {
+
+	
+	
+	
+		
+}
