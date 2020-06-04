828b954ab0992d3e764d772bc728bacea70c8d77
ckadl0118@gmail.com <ckadl0118@gmail.com>
2020-06-04 16:20:13 +0900

    2020.06.04 사용자 : 김참이 내   용 : user,userD, login sql 수정

--- NewStart/src/main/java/com/start/pro/security/Sc_User.java
+++ NewStart/src/main/java/com/start/pro/security/Sc_User.java
@@ -1,5 +1,71 @@
 package com.start.pro.security;
 
-public class Sc_User {
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class Sc_User implements UserDetails {
+
+	
+	private String user_seq;
+	private String user_pw;
+	private String user_grade;
+	private String user_type;
+
+	public Sc_User() {}
+
+	public Sc_User(String user_seq, String user_pw, String user_grade, String user_type) {
+		super();
+		this.user_seq = user_seq;
+		this.user_pw = user_pw;
+		this.user_grade = user_grade;
+		this.user_type = user_type;
+	}
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		ArrayList<GrantedAuthority> auth = new ArrayList<GrantedAuthority>();
+		System.out.println("유저권한은?"+"ROLE_"+user_grade);
+		auth.add(new SimpleGrantedAuthority("ROLE_"+user_grade));
+		return auth;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return user_seq;
+	}
+
+	//계정이 만료되지 않았는 지 리턴한다. (true: 만료안됨)
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	//계정이 잠겨있지 않았는 지 리턴한다. (true: 잠기지 않음)
+	@Override
+	public boolean isAccountNonLocked() {
+		return (!user_type.equalsIgnoreCase("L")) ? true : false;
+	}
+
+	//비밀번호가 만료되지 않았는 지 리턴한다. (true: 만료안됨)
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	// 계정이 활성화(사용가능)인 지 리턴한다. (true: 활성화)
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 
 }
