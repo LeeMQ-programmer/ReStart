2e8543b25ebd9c4dae39b03640c94b7ef1f5c289
ckadl0118@gmail.com <ckadl0118@gmail.com>
2020-06-07 02:47:49 +0900

    2020.06.07 사용자 : 김참이 내   용 : 자바메일 빈 추가

---
+++ NewStart/src/main/java/com/start/pro/email/AsyncTask_SendEmail.java
@@ -0,0 +1,13 @@
+package com.start.pro.email;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AsyncTask_SendEmail {
+
+	
+	private final String setFrom = "ckadl0118@gmail.com";
+	
+	
+	
+}
